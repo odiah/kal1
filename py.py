0cf64da10412b9b7d639434265e5efc022a2985b
Kal <40569610+odiah@users.noreply.github.com>
2018-07-03 02:21:28 +0100

    Add files via upload
    Nothing to say here.

---
+++ py.py
@@ -0,0 +1,9 @@
+print("We're about to find the average of just three numbers")
+print("This is my first official python code in up to three years, by the way")
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+num3 = int(input("Enter number 3: "))
+avg = (num1+num2+num3)
+print("The average of the three numbers is: ", avg)
+
+    
