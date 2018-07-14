b1fd08602d7a1db179968d11adbbd30dda4e49f3
Kal <40569610+odiah@users.noreply.github.com>
2018-07-14 12:59:22 +0100

    Update py.py

--- py.py
+++ py.py
@@ -3,7 +3,13 @@ print("This is my first official python code in up to three years, by the way")
 num1 = int(input("Enter number 1: "))
 num2 = int(input("Enter number 2: "))
 num3 = int(input("Enter number 3: "))
-avg = (num1+num2+num3)
+avg = (num1+num2+num3)/3
 print("The average of the three numbers is: ", avg)
+step2 = [num3,num1,num2]
+for lowest in step2:
+    if(num1>num2) and (num1>num3):
+        num1 = lowest
+        if(num2>num1) and (num2>num3):
+            num2 = lowest
 
-    
+print("\n BTW, the highest of the numbers you typed in is: ", lowest)
